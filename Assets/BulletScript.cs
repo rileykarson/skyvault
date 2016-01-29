c10c393e76dd602da31c67802cdcb274e3a6fda1
Riley Karson <Riley Karson>
2016-01-29 13:05:29 -0500

    Added BulletScript

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	float speed = 16f;
+	// Use this for initialization
+	void Start () {
+		Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		pz.z = 0;
+		var ratiox = pz.x / (Mathf.Abs(pz.x) + Mathf.Abs(pz.y));
+		var ratioy = pz.y / (Mathf.Abs(pz.x) + Mathf.Abs(pz.y));
+		var rigi = GetComponent<Rigidbody2D>();
+		rigi.velocity = new Vector2 (speed*ratiox, speed*ratioy);
+		Debug.Log (speed * ratiox + " " + speed * ratioy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Gets called when the object goes out of the screen
+	void OnBecameInvisible() {  
+		// Destroy the bullet 
+		Destroy(gameObject);
+	}
+}
