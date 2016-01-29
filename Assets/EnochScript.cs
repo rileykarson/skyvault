cd421b7de8dccced46a54859ee1f356c40520554
Riley Karson <Riley Karson>
2016-01-29 12:28:19 -0500

    Added EnochScript

---
+++ Assets/EnochScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnochScript : MonoBehaviour {
+
+	float speed = 8f;
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey( KeyCode.Q )) {
+			Instantiate(bullet, transform.position, Quaternion.identity);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(-Vector2.right * speed * Time.deltaTime);    
+		}
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector2.right * speed * Time.deltaTime);   
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector2.up * speed * Time.deltaTime);
+		}
+		else if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(-Vector2.up * speed * Time.deltaTime);  
+		}
+	}
+}
