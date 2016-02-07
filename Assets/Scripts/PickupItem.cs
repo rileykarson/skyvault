8a0477297d9c99e66caf10f45f40421d7785f758
Enoch Tam <et.enoch@gmail.com>
2016-02-07 17:31:00 -0500

    Pickup item script

---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupItem : MonoBehaviour {
+
+	public static int pickupCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D collision){
+		pickupCount++;
+		print("number picked up: " + pickupCount);
+		Destroy (gameObject);
+	}
+}
