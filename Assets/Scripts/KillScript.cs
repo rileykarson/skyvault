e0e05e083d02c6c2c4a96d6e8ff6232ca35c4727
Enoch Tam <et.enoch@gmail.com>
2016-02-07 17:14:35 -0500

    Kill script

---
+++ Assets/Scripts/KillScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Application.LoadLevel ("startScreen");
+	}
+}
