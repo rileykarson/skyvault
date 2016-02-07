bd521eef81a74e22119212ca1b198cd19bf106c4
Riley Karson <rileykarson@users.noreply.github.com>
2016-02-07 17:42:42 -0500

    Added floors and made physics look real.

--- Assets/Scripts/RayAffectedScript.cs
+++ Assets/Scripts/RayAffectedScript.cs
@@ -2,29 +2,44 @@
 using System.Collections;
 
 public class RayAffectedScript : MonoBehaviour {
+	
+	public float midYOffset = 2.5f;
+	public float endYOffset = 5;
 
 	public float flippedY = 5;
 
-	private bool flipped = false;
+	private float initialY;
+	private float midY;
+	private float endY;
+
+	private int state = 1;
+
 	// Use this for initialization
 	void Start () {
-	
+		initialY = transform.position.y;
+		midY = transform.position.y + midYOffset;
+		endY = transform.position.y + endYOffset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (state == -1 && transform.position.y != endY) {
+			transform.Translate (new Vector2 (0, 1));
+		} else if (state == 1 && transform.position.y != initialY) {
+			transform.Translate (new Vector2 (0, -1));
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "RayShot") {
-        	if (flipped) {
-        		transform.Translate(new Vector2(0, flippedY) * -1);
+        	if (state == 1) {
+        		//transform.Translate(new Vector2(0, flippedY));
+				state = -1;
         	} else {
-        		transform.Translate(new Vector2(0, flippedY));
+        		//transform.Translate(new Vector2(0, flippedY) * -1);
+				state = 1;
         	}
-        	flipped = !flipped;	
         }
     }
 }
