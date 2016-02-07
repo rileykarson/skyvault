9dc115baf0f8dc65ec3ee4ba7275022cbc903d73
Riley Karson <rileykarson@users.noreply.github.com>
2016-02-07 17:50:09 -0500

    Rigid Gravity flips

---
+++ Assets/Scripts/RigidRayAffected.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidRayAffected : MonoBehaviour {
+
+	private Rigidbody2D body;
+	// Use this for initialization
+	void Start () {
+		body = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "RayShot") {
+			body.gravityScale *= -1;
+		}
+	}
+}
