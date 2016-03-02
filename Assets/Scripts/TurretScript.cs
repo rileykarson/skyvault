c7e1bd620a72a13251fa2c9dc92615527c89cd14
Riley Karson <rileykarson@users.noreply.github.com>
2016-03-02 15:23:04 -0500

    Fixed Turret Hype

--- Assets/Scripts/TurretScript.cs
+++ Assets/Scripts/TurretScript.cs
@@ -8,12 +8,15 @@ public class TurretScript : MonoBehaviour {
 	public GameObject bullet;
 	private Animator m_Anim;
 	public GameObject playerObject;
+	public bool fixedTurret = false;
 
 	// Use this for initialization
 	void Start () {
 		bulletCooldown = 0;
+		if (!fixedTurret) {
+			m_Anim = GetComponent<Animator>();
+		}
 		playerObject = GameObject.Find("player-character");
-		m_Anim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -23,11 +26,14 @@ public class TurretScript : MonoBehaviour {
 
 	void FixedUpdate()
 	{
+		float xCoord = 0;
+		float yCoord = 0;
+		float zCoord = 0;
+		bool turretEnabled = false;
+
 		Vector3 playerPos = playerObject.transform.position;
 		Vector3 line = playerPos - transform.position;
 		Vector3 forward = transform.forward;
-
-		bool turretEnabled = Vector3.Distance (playerPos, transform.position) < 10;
 
 		Vector2 line2D;
 		line2D.x = line.x;
@@ -38,49 +44,53 @@ public class TurretScript : MonoBehaviour {
 		forward2D.y = forward.y;
 
 		float angle = Vector2.Angle(line2D, forward2D);
+		if (fixedTurret == true) {
+			xCoord = -0.6f;
+			yCoord = 0.1f;
+			turretEnabled = (angle > 135) && (angle < 180);
+		} else {
 
-		float xCoord = 0;
-		float yCoord = 0;
-		float zCoord = 0;
+			turretEnabled = Vector3.Distance (playerPos, transform.position) < 10;
 
-		if (line2D.y < 0) {
-			if (line2D.x > 0) {
-				xCoord = 0.48f;
-				yCoord = 0.4f;
-				m_Anim.SetFloat ("Blend", 7);
-			} else {
+			if (line2D.y < 0) {
+				if (line2D.x > 0) {
+					xCoord = 0.48f;
+					yCoord = 0.4f;
+					m_Anim.SetFloat ("Blend", 7);
+				} else {
+					xCoord = -0.48f;
+					yCoord = 0.4f;
+					m_Anim.SetFloat ("Blend", 1);
+				}
+			} else if (angle > 135) {
 				xCoord = -0.48f;
 				yCoord = 0.4f;
-				m_Anim.SetFloat ("Blend", 1);
+				m_Anim.SetFloat("Blend", 1);
+			} else if (angle > 117) {
+				xCoord = -0.32f;
+				yCoord = 0.46f;
+				m_Anim.SetFloat("Blend", 2);
+			} else if (angle > 99) {
+				xCoord = -0.16f;
+				yCoord = 0.52f;
+				m_Anim.SetFloat("Blend", 3);
+			} else if (angle > 81) {
+				xCoord = 0;
+				yCoord = 0.58f;
+				m_Anim.SetFloat("Blend", 4);
+			} else if (angle > 63) {
+				xCoord = 0.16f;
+				yCoord = 0.52f;
+				m_Anim.SetFloat("Blend", 5);
+			} else if (angle > 45) {
+				xCoord = 0.32f;
+				yCoord = 0.46f;
+				m_Anim.SetFloat("Blend", 6);
+			} else {
+				xCoord = 0.48f;
+				yCoord = 0.4f;
+				m_Anim.SetFloat("Blend", 7);
 			}
-		} else if (angle > 135) {
-			xCoord = -0.48f;
-			yCoord = 0.4f;
-			m_Anim.SetFloat("Blend", 1);
-		} else if (angle > 117) {
-			xCoord = -0.32f;
-			yCoord = 0.46f;
-			m_Anim.SetFloat("Blend", 2);
-		} else if (angle > 99) {
-			xCoord = -0.16f;
-			yCoord = 0.52f;
-			m_Anim.SetFloat("Blend", 3);
-		} else if (angle > 81) {
-			xCoord = 0;
-			yCoord = 0.58f;
-			m_Anim.SetFloat("Blend", 4);
-		} else if (angle > 63) {
-			xCoord = 0.16f;
-			yCoord = 0.52f;
-			m_Anim.SetFloat("Blend", 5);
-		} else if (angle > 45) {
-			xCoord = 0.32f;
-			yCoord = 0.46f;
-			m_Anim.SetFloat("Blend", 6);
-		} else {
-			xCoord = 0.48f;
-			yCoord = 0.4f;
-			m_Anim.SetFloat("Blend", 7);
 		}
 		if (bulletCooldown == 0) {
 			if (turretEnabled) {
