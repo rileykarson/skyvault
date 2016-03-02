319a14106d6bedea47ac3765fd13587f0ee99d35
jnlycklama <jnlycklama@gmail.com>
2016-03-02 14:10:01 -0500

    end screen

--- Assets/Scripts/KillScript.cs
+++ Assets/Scripts/KillScript.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 
 public class KillScript : MonoBehaviour {
 
-	void OnCollisionEnter2D(Collision2D collision)
+	IEnumerator OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Hazard")) {
+			Application.LoadLevel ("YouLose");
+			yield return new WaitForSeconds(5);
 			Application.LoadLevel("StartScreen");
 		}
 	}
---
+++ Assets/Scripts/onLose.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onLose : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(5);
+		Application.LoadLevel("StartScreen");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
