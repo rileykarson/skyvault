822f6111e232c058bb20762c2d2585f632aa48e7
Riley Karson <rileykarson@users.noreply.github.com>
2016-03-10 16:52:36 -0500

    Added god mode flag

--- Assets/Scripts/KillScript.cs
+++ Assets/Scripts/KillScript.cs
@@ -4,21 +4,27 @@ using UnityEngine.UI;
 
 public class KillScript : MonoBehaviour {
 
+	public bool godmode = false;
+
 	IEnumerator OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Hazard")) {
-			Application.LoadLevel ("YouLose");
-			yield return new WaitForSeconds(5);
-			Application.LoadLevel("StartScreen");
+		if (!godmode) {
+			if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Hazard")) {
+				Application.LoadLevel ("YouLose");
+				yield return new WaitForSeconds (5);
+				Application.LoadLevel ("StartScreen");
+			}
 		}
 	}
 
 	IEnumerator OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.gameObject.layer == LayerMask.NameToLayer("Hazard")) {
-			Application.LoadLevel ("YouLose");
-			yield return new WaitForSeconds(5);
-			Application.LoadLevel("StartScreen");
+		if (!godmode) {
+			if (collision.gameObject.layer == LayerMask.NameToLayer ("Hazard")) {
+				Application.LoadLevel ("YouLose");
+				yield return new WaitForSeconds (5);
+				Application.LoadLevel ("StartScreen");
+			}
 		}
 	}
 }
