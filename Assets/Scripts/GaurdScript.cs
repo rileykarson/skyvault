6c04898b51370e5f5717aaa5d94b99fe713dfee2
jnlycklama <jnlycklama@gmail.com>
2016-03-16 13:43:59 -0400

    guard anim

--- Assets/Scripts/GaurdScript.cs
+++ Assets/Scripts/GaurdScript.cs
@@ -11,15 +11,23 @@ public class GaurdScript : MonoBehaviour {
 	public GameObject bullet;
 	public GameObject particle;
 
+	private Rigidbody2D m_Rigidbody2D;
+	private Animator animator;
+
 	private Vector3 frometh;
 	private Vector3 untoeth;
 	private float secondsForOneLength = 8f;
+
+	private float old_x;
 
 	// Use this for initialization
 	void Start () {
 		frometh = transform.position;
 		untoeth = transform.position;
 		untoeth.x += 11;
+		animator = this.GetComponent<Animator>();
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		old_x = transform.position.x;
 	}
 
 	
@@ -28,7 +36,17 @@ public class GaurdScript : MonoBehaviour {
         
 		transform.position = Vector3.Lerp(frometh, untoeth,Mathf.SmoothStep(0f,1f,Mathf.PingPong(Time.time/secondsForOneLength, 1f)));
 
-
+		if (old_x > transform.position.x) {
+			animator.SetInteger ("Direction", 3);
+			//animator.SetFloat ("Direction", 2);
+		} else if (old_x < transform.position.x) {
+			animator.SetInteger ("Direction", 2);
+			//animator.SetFloat ("Direction", 1);
+		} else {
+			animator.SetInteger ("Direction", 1);
+		}
+		old_x = transform.position.x;
+			
 		if(Vector3.Distance(transform.position, player.position) < distance){
 			playerCloseEnough = true;
 
