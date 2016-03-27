921ec5fc68f73c4a5f6ef22db03c74172dc286f2
Riley Karson <rileykarson@users.noreply.github.com>
2016-03-27 15:43:03 -0400

    Alexis

---
+++ Assets/AlexisScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlexisScript : MonoBehaviour {
+
+	public GameObject audioObj;
+	public GameObject newImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision){
+		Instantiate (audioObj, transform.position, Quaternion.identity);
+		var render = this.GetComponent<SpriteRenderer> ();
+		Instantiate (newImage, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
